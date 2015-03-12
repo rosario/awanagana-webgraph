a3dfedb63b2c3408f805f2228e19cac6cc1b15ff
Firrincieli Marco (UniCredit) <marco.firrincieli@unicredit.eu>
2015-03-12 15:32:09 +0100

    I hate C as much as you hate Java

---
+++ lein/project.clj
@@ -0,0 +1,6 @@
+(defproject cljcsv "1.0.0-SNAPSHOT"
+  :description "awanagana"
+  :dependencies
+    [[org.clojure/clojure "1.6.0"]
+     [it.unimi.dsi/webgraph "3.4.3"]]
+  :main it.unimi.dsi.webgraph.BVGraph)
